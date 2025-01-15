92a090d0dafe750d0dfa99667660e16515837a60
GitkcExpert <kcossettini@rogers.com>
2025-01-15 11:31:41 -0500

    NC Project Code 2 Progress: Compatibility

--- Games/3D Minesweeper/src/precomp.h
+++ Games/3D Minesweeper/src/precomp.h
@@ -1,18 +1,8 @@
 #ifndef PRECOMP_H
 #define PRECOMP_H
 
-#define RAYLIB_NAMESPACE_BEGIN namespace rl {
-#define RAYLIB_NAMESPACE_END }
-
-#include <winsock2.h>
-
-#undef DrawText
-#undef LoadImage
-
-RAYLIB_NAMESPACE_BEGIN
 #include "raylib.h"
 #include "raymath.h"
-RAYLIB_NAMESPACE_END
 
 namespace sst
 {
@@ -21,22 +11,22 @@ namespace sst
 
     inline int cx(int x)
     {
-        return (((float)rl::GetScreenWidth())/baseX) * (float)x;
+        return (((float)GetScreenWidth())/baseX) * (float)x;
     }
 
     inline int cy(int y)
     {
-        return (((float)rl::GetScreenHeight())/baseY) * (float)y;
+        return (((float)GetScreenHeight())/baseY) * (float)y;
     }
 
     inline float cxf(float x)
     {
-        return (((float)rl::GetScreenWidth())/baseX) * x;
+        return (((float)GetScreenWidth())/baseX) * x;
     }
 
     inline float cyf(float y)
     {
-        return (((float)rl::GetScreenHeight())/baseY) * y;
+        return (((float)GetScreenHeight())/baseY) * y;
     }
 }
 
